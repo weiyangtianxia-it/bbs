98840edc1a2ed1e73325bc58db3046e94215b589
Seamile <lanhuermao@gmail.com>
2018-07-06 10:56:23 +0800

    完成了创建、阅读页

--- bbs/settings.py
+++ bbs/settings.py
@@ -25,18 +25,17 @@ SECRET_KEY = 'zqo%ab(@f4xlu)1=o3es5f7)bntpnjzr$ea(kpun$@kc*yzky5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'post',
 ]
 
 MIDDLEWARE = [
@@ -44,9 +43,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'bbs.urls'
